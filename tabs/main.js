55fa57aeef6d3aab48fdd8df4613762cb07afc27
Mohammad-Taizul-Islam <m.taizul1045@gmail.com>
2025-10-13 21:43:56 +0600

    tabs with animations

---
+++ tabs/main.js
@@ -0,0 +1,22 @@
+const opencity = (event,cityName)=>{
+    let i,tabcontent,tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+
+    for(i=0;i<tabcontent.length;i++){
+        tabcontent[i].style.display = "none";
+    }
+
+
+    tablinks = document.getElementsByClassName("tablinks");
+
+    for(i=0;i<tablinks.length;i++){
+        tablinks[i].className = tablinks[i].className.replace(" active", " ");
+
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
